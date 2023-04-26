34ab686bb85c4b2d2fc0e94f87dfb009022d0668
mtauhidul <mislam.tauhidul@gmail.com>
2023-04-27 02:34:14 +0600

    port added

--- server.js
+++ server.js
@@ -117,4 +117,7 @@ app.post('/create-checkout-session', async (req, res) => {
   });
 });
 
-app.listen(4000, () => console.log('Running on port 4000'));
+app.listen(
+  process.env.PORT || 5000,
+  console.log(`Server running on port ${process.env.PORT || 5000}`)
+);
