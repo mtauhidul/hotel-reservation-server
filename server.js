4e237e6553d0c930fc93c75d4717c2f4fab14292
mtauhidul <mislam.tauhidul@gmail.com>
2023-04-27 02:31:37 +0600

    updates of base path

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const { fulfillOrder } = require("./webhook");
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const { fulfillOrder } = require('./webhook');
+const { resolveAny } = require('dns');
 
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(express.static(path.resolve(__dirname, "../client", "wwwroot", "app")));
+app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, '../client', 'wwwroot', 'app')));
 
 const endpointSecret =
-  "whsec_7e7a8a249df8ed2082a48d3e2e0e220fcce2e4b4fb6b9dc2fc5511d3320d32f2";
+  'whsec_7e7a8a249df8ed2082a48d3e2e0e220fcce2e4b4fb6b9dc2fc5511d3320d32f2';
 
-const YOUR_DOMAIN = (process.env.NODE_ENV = "development"
-  ? "http://localhost:5173"
-  : "https://hotel-reservation-system-hrdq-git-rayhan-dev-mtauhidul.vercel.app");
+const YOUR_DOMAIN = (process.env.NODE_ENV = 'development'
+  ? 'http://localhost:5173'
+  : 'https://hotel-reservation-system-hrdq-git-rayhan-dev-mtauhidul.vercel.app');
+
+app.get('/', (req, res) => {
+  res.send('Hotel Reservation System');
+});
 
 app.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
+  '/webhook',
+  express.raw({ type: 'application/json' }),
   (request, response) => {
-    const sig = request.headers["stripe-signature"];
+    const sig = request.headers['stripe-signature'];
 
     let event;
 
@@ -35,12 +40,12 @@ app.post(
 
     // Handle the event
     switch (event.type) {
-      case "payment_intent.succeeded":
+      case 'payment_intent.succeeded':
         const paymentIntent = event.data.object;
         console.log(paymentIntent);
 
         break;
-      case "checkout.session.completed":
+      case 'checkout.session.completed':
         const session = event.data.object;
         console.log(session);
         fulfillOrder(session)
@@ -59,7 +64,7 @@ app.post(
 );
 app.use(express.json());
 
-app.post("/create-checkout-session", async (req, res) => {
+app.post('/create-checkout-session', async (req, res) => {
   const {
     priceId,
     imagesEN,
@@ -79,7 +84,7 @@ app.post("/create-checkout-session", async (req, res) => {
 
   const transformedItem = {
     price_data: {
-      currency: "usd",
+      currency: 'usd',
       unit_amount: Number(+priceEN * 100) * Number(guests),
       product_data: {
         name: titleEN,
@@ -90,9 +95,9 @@ app.post("/create-checkout-session", async (req, res) => {
     quantity: 1,
   };
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
+    payment_method_types: ['card'],
     line_items: [transformedItem],
-    mode: "payment",
+    mode: 'payment',
     success_url: `${YOUR_DOMAIN}/thanks`,
     cancel_url: `${YOUR_DOMAIN}/rooms`,
     metadata: {
@@ -112,4 +117,4 @@ app.post("/create-checkout-session", async (req, res) => {
   });
 });
 
-app.listen(4000, () => console.log("Running on port 4000"));
+app.listen(4000, () => console.log('Running on port 4000'));
