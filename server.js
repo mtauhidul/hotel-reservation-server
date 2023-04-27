db6d431c56396907fa68a37bb3b67eeece403197
mtauhidul <mislam.tauhidul@gmail.com>
2023-04-28 01:31:48 +0600

    update

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/create-checkout-session', async (req, res) => {
     payment_method_types: ['card'],
     line_items: [transformedItem],
     mode: 'payment',
-    success_url: `${host}/thanks${id}}`,
+    success_url: `${host}/thanks`,
     cancel_url: `${host}/rooms`,
     metadata: {
       roomId: id,
