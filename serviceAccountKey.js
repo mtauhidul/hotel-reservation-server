5948b2fab68426f5fe96af0959c3ad0dc2b955b8
mtauhidul <mislam.tauhidul@gmail.com>
2023-05-02 17:00:47 +0600

    service account key hided

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -1,17 +1,14 @@
-const serviceAccount = {
-  type: process.env.FIREBASE_ADMIN_TYPE,
-  project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
-  token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
-  auth_provider_x509_cert_url:
-    process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+const serviceAccountKey = {
+  type: serviceAccount.type,
+  project_id: serviceAccount.project_id,
+  private_key_id: serviceAccount.private_key_id,
+  private_key: serviceAccount.private_key.replace(/\\n/g, '\n'),
+  client_email: serviceAccount.client_email,
+  client_id: serviceAccount.client_id,
+  auth_uri: serviceAccount.auth_uri,
+  token_uri: serviceAccount.token_uri,
+  auth_provider_x509_cert_url: serviceAccount.auth_provider_x509_cert_url,
+  client_x509_cert_url: serviceAccount.client_x509_cert_url,
 };
-
-const serviceAccountKey = serviceAccount.toJSON();
 
 module.exports = serviceAccountKey;
